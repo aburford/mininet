c33d40c80f820f18100de4bd2136f7b7e092f80e
Andrew Burford <andrewsburford@gmail.com>
2017-03-14 19:23:46 -0400

    calculate accuracy for all numbers

--- divisionTest.lua
+++ divisionTest.lua
@@ -2,6 +2,7 @@ require('mininet')
 nums = {}
 count = 0
 isDivisable = {}
+divisor = 4
 for bit1 = 0,1 do
 	for bit2 = 0,1 do
 		for bit3 = 0,1 do
@@ -10,8 +11,8 @@ for bit1 = 0,1 do
 					for bit6 = 0,1 do
 						for bit7 = 0,1 do
 							for bit8 = 0,1 do
-								nums[count] = {bit7,bit8}
-								if count % 4 == 0 then
+								nums[count] = {bit1,bit2,bit3,bit4,bit5,bit6,bit7,bit8}
+								if count % divisor == 0 then
 									isDivisable[count] = {1}
 								else
 									isDivisable[count] = {0}
@@ -27,26 +28,39 @@ for bit1 = 0,1 do
 end
 
 time = os.time()
-print(time)
-math.randomseed(3)
+--print(time)
+math.randomseed(1)
 
-for batchSize = 1, 255 do
-	net = mininet:new({2,3,1},1)
-	iters = 10000
-	for iteration = 1,iters do
-		-- batch by 32
-		i = 1
-		j = i + batchSize
-		repeat
-			net:backPropagate({unpack(nums,i,j)},{unpack(isDivisable,i,j)})
-			j = i + batchSize
-			i = j + 1
-		until j > #nums
-		
-		--[[if iteration % 10 == 0 then
-			print(iteration / iters * 100 .. "%" .. " Time left: " .. os.clock() * (iters / iteration) - os.clock() .. " seconds")
-		end]]
+batchSize = 8
+local net = mininet:new({8,14,1},1)
+local iters = 10000
+for iteration = 1,iters do
+	-- batch by 32
+	i = 0
+	j = i + batchSize - 1
+	repeat
+		net:backPropagate({unpack(nums,i,j)},{unpack(isDivisable,i,j)})
+		i = j + 1
+		j = i + batchSize - 1
+	until j > #nums
+	
+	if iteration % 1000 == 0 then
+		print(iteration / iters * 100 .. "%" .. " Time left: " .. os.clock() * (iters / iteration) - os.clock() .. " seconds")
 	end
-	net:backPropagate({nums[120]},{{1}})
-	print(batchSize .. "," .. net.layers[3].nodes[1].values[1])
 end
+zeroesCorrect = 0
+onesCorrect = 0
+totalOnes = 0
+for count = 0,#nums do
+	local guess = net:predictOutputs({nums[count]})[1][1]
+	local answer = 0
+	if count % divisor == 0 then
+		answer = 1
+		totalOnes = totalOnes + 1
+	end
+	print(answer .. " | " .. guess)
+	if answer == 1 and guess > 0.5 then onesCorrect = onesCorrect + 1 end
+	if answer == 0 and guess < 0.5 then zeroesCorrect = zeroesCorrect + 1 end
+end
+print("Accuracy for guessing ones: " .. onesCorrect / totalOnes * 100 .. "%")
+print("Accuracy for guessing zeroes: " .. zeroesCorrect / (#nums + 1 - totalOnes) * 100 .. "%")
